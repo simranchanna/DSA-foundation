0bd4457dc8237b78d3f1fd9e09e8857dd46b1f96
simranchanna <simranchanna2000@gmail.com>
2020-12-18 16:57:51 +0530

    add files in Recursion

---
+++ Recursion/Recursion Backtracking/floodfill.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int maze[][] = new int[n][m];
+        boolean visited[][] = new boolean[n][m]; //by default false
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                maze[i][j] = scn.nextInt();
+            }
+        }
+        floodfill(maze, 0, 0, "", visited);
+    }
+
+    public static void floodfill(int[][] maze, int row, int col, String psf, boolean[][] visited) {
+        int n = maze.length;
+        int m = maze[0].length;
+        //base case
+        if (row == n - 1 && col == m - 1) {
+            System.out.println(psf);
+            return;
+        }
+
+        visited[row][col] = true;
+        //top move
+        if (row > 0) {
+            if (!visited[row - 1][col] && maze[row - 1][col] == 0) {
+                floodfill(maze, row - 1, col, psf + "t", visited);
+            }
+        }
+        //left move
+        if (col > 0) {
+            if (!visited[row][col - 1] && maze[row][col - 1] == 0) {
+                floodfill(maze, row, col - 1, psf + "l", visited);
+            }
+        }
+        //down move
+        if (row < n - 1) {
+            if (!visited[row + 1][col] && maze[row + 1][col] == 0) {
+                floodfill(maze, row + 1, col, psf + "d", visited);
+            }
+        }
+        //right move
+        if (col < m - 1) {
+            if (!visited[row][col + 1] && maze[row][col + 1] == 0) {
+                floodfill(maze, row, col + 1, psf + "r", visited);
+            }
+        }
+        visited[row][col] = false;
+    }
+}
