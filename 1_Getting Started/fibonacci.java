857e24e4feabb0d5460bc58274157c7e706d6fbc
simranchanna <simranchanna2000@gmail.com>
2021-01-23 16:21:52 +0530

    add files

---
+++ 1_Getting Started/fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int a = 0, b = 1, c;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || i == 1)
+                System.out.println(i);
+            else {
+                c = a + b;
+                System.out.println(c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
