5663287cc42303341a5e44e43cf56db12952801b
simranchanna <simranchanna2000@gmail.com>
2021-03-13 15:07:07 +0530

    add files

--- 10_Binary Search Tree/BSTclass.java
+++ 10_Binary Search Tree/BSTclass.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BSTclass {
     public class Node{
         int data;
@@ -110,5 +112,48 @@ public class BSTclass {
                 return curr.data;
         }
         return -1;
+    }
+
+    public static void pir(Node node, int d1, int d2) {
+        if(node == null)
+            return;
+        if(node.data >= d1 && node.data <= d2){
+            pir(node.left, d1, d2);
+            System.out.println(node.data);
+            pir(node.right, d1, d2);
+        }    
+        else if(node.data < d1)
+            pir(node.right, d1, d2);
+        else
+            pir(node.left, d1, d2);
+    }
+
+    public static void inorder(Node root, ArrayList<Integer> ans){
+        if(root == null)
+            return;
+        inorder(root.left,ans);
+        ans.add(root.data);    
+        inorder(root.right,ans);    
+    }
+
+    public static void targetSum(Node node, int tar){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        inorder(node, list);
+        int i = 0;
+        int j = list.size() - 1;
+        while(i < j){
+            int n1 = list.get(i);
+            int n2 = list.get(j);
+            int sum = n1 + n2;
+            if(sum > tar)
+                j--;
+            else if(sum < tar)
+                i++;
+            else{
+                System.out.println(n1 + " " + n2);
+                i++;
+                j--;
+            }          
+        }
     }
 }
