100d228f003ae607e5b5c39f44b82580067c048f
simranchanna <simranchanna2000@gmail.com>
2021-06-05 22:40:40 +0530

    add files

---
+++ 0_LevelUp/Advanced Linked List/subtractTwoLists.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Main {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode subtractTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+        int borrow = 0;
+        while(c1 != null || c2 != null){
+            int diff = borrow + ;
+        }
+        return null;
+    }
+
+    // InFput_code===================================================
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static ListNode makeList(int n) {
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head1 = makeList(scn.nextInt());
+        ListNode head2 = makeList(scn.nextInt());
+
+        ListNode ans = subtractTwoNumbers(head1, head2);
+        printList(ans);
+    }
+
+}
